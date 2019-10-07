a34449eb10f654ae37586b8154acf6a3537cd51a
Yigong Hu <hyigong1@jhu.edu>
2019-10-07 11:49:26 -0400

    Add syscall tracker

--- src/s2e/Plugins/ConfigurationAnalysis/InstructionTracker.cpp
+++ src/s2e/Plugins/ConfigurationAnalysis/InstructionTracker.cpp
@@ -17,18 +17,19 @@ namespace s2e {
         private:
             int m_count;
             uint64_t entry_point;
-            bool evoked;
             bool regiesterd;
             stack<pair<uint64_t,clock_t>> call_list;
 
+
         public:
             list<pair<uint64_t,double>> call_latency;
-
+            bool test_flag;
+            clock_t begin ;
             InstructionTrackerState() {
                 m_count = 0;
                 entry_point = 0;
-                evoked = false;
                 regiesterd = false;
+                test_flag = false;
             }
 
             virtual ~InstructionTrackerState() {}
@@ -57,14 +58,6 @@ namespace s2e {
                 return entry_point;
             }
 
-            void setEvokeState(S2EExecutionState* state, bool evostate) {
-                evoked = evostate;
-            }
-
-            bool getEvokeState() {
-                return evoked;
-            }
-
             void setRegState(bool state) {
                 regiesterd = state;
             }
@@ -84,7 +77,7 @@ namespace s2e {
                 pair<uint64_t,clock_t> temp = call_list.top();
                 call_list.pop();
                 clock_t end = clock();
-                call_latency.push_back(make_pair(temp.first,double(end - temp.second) / CLOCKS_PER_SEC));
+                call_latency.push_back(make_pair(temp.first,double(end - temp.second) / (CLOCKS_PER_SEC/1000)));
                 return true;
             }
 
@@ -98,12 +91,16 @@ namespace s2e {
 
         void InstructionTracker::initialize() {
             m_address = (uint64_t) s2e()->getConfig()->getInt(getConfigKey() + ".addressToTrack");
-            m_flag = s2e()->getConfig()->getBool(getConfigKey() + ".profileAllFunction");
+            is_profileAll = s2e()->getConfig()->getBool(getConfigKey() + ".profileAllFunction");
+            is_traceSyscall = s2e()->getConfig()->getBool(getConfigKey() + ".traceSyscall");
             // This indicates that our plugin is interested in monitoring instruction translation.
             // For this, the plugin registers a callback with the onTranslateInstruction signal.
             s2e()->getCorePlugin()->onTranslateInstructionStart.connect(
                     sigc::mem_fun(*this, &InstructionTracker::onTranslateInstruction));
 
+            if (is_traceSyscall) {
+                s2e()->getCorePlugin()->onException.connect (sigc::mem_fun(*this, &InstructionTracker::onException));
+            }
         }
 
         void InstructionTracker::onTranslateInstruction(ExecutionSignal *signal,
@@ -113,7 +110,7 @@ namespace s2e {
             DECLARE_PLUGINSTATE(InstructionTrackerState, state);
            // uint64_t entryPoint = plgState->getEntryPoint();
 
-            if(m_flag) {
+            if(is_profileAll) {
                 // When s2e starts at the entry of our module, we begin to analyse function
                 m_monitor = s2e()->getPlugin<FunctionMonitor>();
                 if(plgState->getRegState()) {
@@ -127,31 +124,41 @@ namespace s2e {
         }
 
 
+        void InstructionTracker::onException (S2EExecutionState* state, unsigned exception_idx, uint64_t pc) {
+            //  s2e()->getDebugStream() << " >> Exception on pc: 0x" << hexval(pc)  << " exception_idx: " <<exception_idx << "(0x" << hexval(exception_idx) << ")\n";
+            // 0x80 128d is software interrupt
+            //  DECLARE_PLUGINSTATE (SyscallCatchState, state);
+            if (exception_idx == 0x80) {
+                // get eax register
+                uint64_t int_num = 0;
+                int_num = int_num & 0xffffffff;
+                onSyscall (state, pc, int_num);
+            }
+            return;
+        }
+
+        void InstructionTracker::onSyscall (S2EExecutionState* state, uint64_t pc, uint32_t sysc_number) {
+            // the kernel doesn't make system calls, so getPid () is accurate here
+            getInfoStream(state) << "Syscall " << hexval(pc) << "\n";
+            return;
+        }
+
+
         void InstructionTracker::functionCallMonitor(S2EExecutionState* state, FunctionMonitorState* fms) {
             DECLARE_PLUGINSTATE(InstructionTrackerState, state);
             uint64_t addr = state->regs()->getPc();
-
-            uint64_t entryPoint = plgState->getEntryPoint();
-            if (m_address+entryPoint == addr) {
-                plgState->increment();
-            }
-            FUNCMON_REGISTER_RETURN(state, fms, InstructionTracker::functionRetMonitor);
-            //getDebugStream(state) << "Evoke call monitor at state " << state->getID() << ", pc:" << hexval(addr) << "\n";
+            
             plgState->functionStart(addr);
-            plgState->setEvokeState(state, true);
+
+            FUNCMON_REGISTER_RETURN(state, fms, InstructionTracker::functionRetMonitor);
+
         }
 
         void InstructionTracker::functionRetMonitor(S2EExecutionState *state) {
-            // ...
-            // Perform here any analysis or state manipulation you wish
-            // ...
             DECLARE_PLUGINSTATE(InstructionTrackerState, state);
-            //uint64_t addr = state->regs()->getPc();
             if (!plgState->functionEnd()){
                 getDebugStream(state) << "No Caller\n";
             }
-            //getDebugStream(state) << "Evoke return monitor at state " << state->getID() << " pc:" <<  hexval(addr)  << "\n";
-
         }
 
 
@@ -162,11 +169,8 @@ namespace s2e {
 
         void InstructionTracker::functionForEach(S2EExecutionState *state) {
             DECLARE_PLUGINSTATE(InstructionTrackerState, state);
-            for (auto iterator = plgState->call_latency.begin(); iterator != plgState->call_latency.end(); ++iterator) {
-                //if (iterator->second >= 0.001) {
-                    getInfoStream(state) << "Function " << hexval(iterator->first) << " runs " << iterator->second << "s\n";
-               // }
-            }
+            for (auto iterator = plgState->call_latency.begin(); iterator != plgState->call_latency.end(); ++iterator)
+                getInfoStream(state) << "Function " << hexval(iterator->first-plgState->getEntryPoint()+m_address) <<" runs " << iterator->second << "ms\n";
         }
 
         void InstructionTracker::setEntryPoint(S2EExecutionState *state,uint64_t entry_point) {
@@ -178,11 +182,9 @@ namespace s2e {
 
         void InstructionTracker::handleOpcodeInvocation(S2EExecutionState *state, uint64_t guestDataPtr, uint64_t guestDataSize) {
             DECLARE_PLUGINSTATE(InstructionTrackerState, state);
-
             m_monitor = s2e()->getPlugin<FunctionMonitor>();
-            if(plgState->getRegState()) {
+            if(plgState->getRegState())
                 return;
-            }
             callSignal = m_monitor->getCallSignal(state, -1, -1);
             callSignal->connect(sigc::mem_fun(*this, &InstructionTracker::functionCallMonitor));
 
